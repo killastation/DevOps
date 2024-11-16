3d6563e097f534b944fdaedd2eae47c1cc541cd0
macnaer <trofimchuk.an@gmail.com>
2024-11-16 12:54:21 +0200

    Update volume size

--- Diploma/1. Terraform/start.tf
+++ Diploma/1. Terraform/start.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "EC2-Instance" {
   // Create main disk
   ebs_block_device {
     device_name = "/dev/sda1"
-    volume_size = 15
+    volume_size = 30
     tags = {
       "name" = "root disk"
     }
